45ab9aa270693203ff327e66552e64ac04c8d517
Zaur <zaur.quseynzade@gmail.com>
2022-12-23 16:50:34 +0400

    edited admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,19 +1,41 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 
 from .models import News, Category
 
+
 class NewAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category','creates_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'category', 'creates_at',
+                    'updated_at', 'is_published', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'category')
+    fields = ('title', 'category', 'content', 'photo', 'get_photo',
+              'is_published', 'views', 'creates_at', 'updated_at')
+
+    readonly_fields = ('views', 'creates_at', 'updated_at', 'get_photo')
+    save_on_top = True
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="95">')
+        else:
+            return ''
+
+    get_photo.short_description = 'Миниатюра'
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     list_display_links = ('id', 'title')
     search_fields = ('title',)
 
+
 admin.site.register(News, NewAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_header = 'Управление новостями'
+
+admin.site.site_title = 'Управление новостями'
