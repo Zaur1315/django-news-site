6e61774f54d0a98bfa38833133c9ab070440418f
Zaur <zaur.quseynzade@gmail.com>
2022-12-25 16:18:24 +0400

    pagination

--- news/urls.py
+++ news/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('test/', test, name='test'),
     path('', HomeNews.as_view(), name='home'),
     # path('', index, name='home'),
     path('category/<int:category_id>/',
